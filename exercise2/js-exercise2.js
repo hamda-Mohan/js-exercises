278a08204478a43e4e3bf0993f6bf5574ef475f9
hamda-Mohan <duumohamed3@gmail.com>
2026-01-16 19:43:06 +0300

    Submit exercise 2

---
+++ exercise2/js-exercise2.js
@@ -0,0 +1,13 @@
+let a = 15 ;
+let b = "15" ;
+
+// Comparison operators
+console.log(a == b);  // Equal to
+console.log(a != b);  // Not equal to
+console.log(a === b); // Strict equal to
+console.log(a !== b); // Strict not equal to
+console.log(a > b);   // Greater than
+console.log(a < b);   // Less than
+console.log(a >= b);  // Greater than or equal to
+console.log(a <= b);  // Less than or equal to      
+
