7cd99efde06b2b5dc92da73b79b0391c14742a6f
hamda-Mohan <duumohamed3@gmail.com>
2026-01-16 19:18:53 +0300

    Submit exercise 1

---
+++ exercise1/js-exercice1.js
@@ -0,0 +1,14 @@
+let x = 7 ;
+let y = 3 ;
+
+// Arithmetic operators
+
+console.log(x + y); // Addition 10
+console.log(x - y); // Subtraction 4
+console.log(x * y); // Multiplication 21
+console.log(x / y); // Division 2.3333
+console.log(x % y); // Modulus 1
+console.log(x++);   // Post-increment 7
+console.log(++y);   // Pre-increment 4
+console.log(x--);  // Post-decrement 8
+console.log(--y);  // Pre-decrement 3
