1092c3525521519490e8f4ee718666e807e8a082
hamda-Mohan <duumohamed3@gmail.com>
2026-01-17 20:49:29 +0300

    Submit exercise 5

---
+++ exercise5/js-exercise5.js
@@ -0,0 +1,10 @@
+// arrow function
+const add = (x,y) => {
+    return x + y;
+}
+console.log(add(1675,10));
+console.log(add(20,430));
+console.log(add(43,57));
+console.log(add(715,25));
+console.log(add(35,405));
+console.log(add(50,750));
