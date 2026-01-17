a056b3b71b6d65dfe2d2d568fc4906b6c83cd38e
hamda-Mohan <duumohamed3@gmail.com>
2026-01-17 11:12:59 +0300

    Submit exercise 3

---
+++ exercise3/js-exercise3.js
@@ -0,0 +1,7 @@
+let isSunny = false;
+let isWeekend = true;
+
+//logical operators
+console.log(isSunny && isWeekend); // AND 
+console.log(isSunny || isWeekend); // OR
+console.log(!isWeekend); // NOT
