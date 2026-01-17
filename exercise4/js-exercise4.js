59b8e8663c8c21291a8d8758eecdbc00ef11a1aa
hamda-Mohan <duumohamed3@gmail.com>
2026-01-17 20:40:21 +0300

    Submit exercise 4

---
+++ exercise4/js-exercise4.js
@@ -0,0 +1,15 @@
+// declaration function
+function add(x,y){
+    console.log("Sum is: " + (x + y));
+}
+add(5,10);
+add(20,30);
+add(43,57);
+
+// expression function
+const sum  = function(x,y){
+    console.log("Sum is: " + (x + y));
+}
+sum(15,25);
+sum(35,45);
+sum(50,70);
