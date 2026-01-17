a3941718f1d4f128a35ac5113249ff0e5a621a88
hamda-Mohan <duumohamed3@gmail.com>
2026-01-17 21:17:13 +0300

    Submit exercise 7

---
+++ exercise7/js-exercise7.js
@@ -0,0 +1,21 @@
+let colors = ["red", "green", "blue"];
+console.log(colors);
+
+//push method
+colors.push("yellow");
+console.log(colors);
+
+//pop method
+colors.pop();
+console.log(colors);    
+
+//unshift method
+colors.unshift("purple");
+console.log(colors);
+
+//shift method
+colors.shift();
+console.log(colors);
+
+//length property
+console.log(colors.length);
