666f57465672839baefd2821eb9d1cacc5e3ea59
hamda-Mohan <duumohamed3@gmail.com>
2026-01-20 21:28:10 +0300

    Submit exercise 17

---
+++ exercise17/js-exercise17.js
@@ -0,0 +1,11 @@
+let tempareture = 10;
+
+if (tempareture == 0){
+    console.log("very cold day");
+}else  if (tempareture < 15){
+    console.log("cold day");
+} else if (tempareture < 25){
+    console.log("warm day");
+} else {
+    console.log("hot day");
+}
