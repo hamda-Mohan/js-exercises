768efa5dfcf0823df3a3a142586cafc93f242668
hamda-Mohan <duumohamed3@gmail.com>
2026-01-26 15:56:27 +0300

    Submit exercise 29

---
+++ exercise29/js-exercise29.js
@@ -0,0 +1,11 @@
+async function fetchingData() {
+    console.log("start fetchig data");
+
+    const response = (await fetch('data.json'));
+    const data = await response.json();
+    const jsntstr = JSON.stringify(data)
+
+    console.log("response data: ",jsntstr )
+    
+}
+fetchingData()
