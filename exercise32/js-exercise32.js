965a0d44ec157a1ba64886d80cd2edf16ca7621d
hamda-Mohan <duumohamed3@gmail.com>
2026-01-30 00:50:25 +0300

    Submit exercise 32

---
+++ exercise32/js-exercise32.js
@@ -0,0 +1,9 @@
+// querySelector
+const title = document.querySelector("#main-title");
+
+// querySelectorAll
+const paragraph = document.querySelectorAll(".description");
+
+// results 
+console.log("Title:", title);
+console.log("paragraph:", paragraph);
